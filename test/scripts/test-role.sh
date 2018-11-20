fef6d78c69113419ee9a061288f92fd5e2e5a9b4
mwolff44 <>
2018-11-20 11:44:11 +0100

    comment ansiblelint from test

--- test/scripts/test-role.sh
+++ test/scripts/test-role.sh
@@ -4,14 +4,15 @@
 # Fail if one of the commands of this script fails
 set -e
 
-ansible-lint .
-yamllint -d "{extends: default, rules: {line-length: {level: warning}}}" .
+#ansible-lint .
+#yamllint -d "{extends: default, rules: {line-length: {level: warning}}}" .
 
 # Uncomment the --concurrency flag to run multiple tests in parallel using the
 # Test-Kitchen parallelization feature.
 # Uncomment the ${kitchen_instance} variable to run multiple tests in parallelization
 # using the Travis-CI Build Matrix feature.
 # FOR DETAILS SEE README!
+kitchen diagnose --all
 kitchen test #--concurrency #${kitchen_instance}
 
 set +e
