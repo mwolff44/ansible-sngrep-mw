adb39133d183b620a35c3aa72cf7332e060f2b50
mwolff44 <>
2018-11-24 18:45:33 +0100

    full molecule tested

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -6,7 +7,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_sngrep_app_is_installed(host):
-    sngrep = host.package('sngrep')
+@pytest.mark.parametrize('pkg', [
+  'sngrep'
+])
+def test_pkg_app_is_installed(host, pkg):
+    package = host.package(pkg)
 
-    assert sngrep.is_installed
+    assert package.is_installed
